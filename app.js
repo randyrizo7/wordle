39fcfeeafa19a2bd4102698117c5d32b72fb053d
randyrizo7 <randyrizopdx@gmail.com>
2022-02-25 18:28:35 -0800

    added html css and js files

---
+++ app.js
@@ -0,0 +1,6 @@
+const tileDisplay = document.querySelector('.tile-container');
+const keyboard = document.querySelector('.key-container');
+
+const keys =  [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'
+]
